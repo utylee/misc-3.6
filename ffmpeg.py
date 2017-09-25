4077040871525f44f3d53831d4e6cc2594ca4a53
utylee <utylee@gmail.com>
2017-09-25 13:52:50 +0900

    chgd translate to support eng/jpn

--- ffmpeg.py
+++ ffmpeg.py
@@ -5,5 +5,5 @@ import subprocess
 
 subprocess.run('/mnt/e/Down/ffmpeg-3.2.4/bin/ffmpeg.exe -i \
         c:/Users/utylee/11.mp4 -c:a copy -c:v copy -g 1 \
-        -ss 00:52:56 \
-        -to 00:55:54 out.mp4', shell=True)
+        -ss 00:59:38 \
+        -to 01:01:25 out.mp4', shell=True)
