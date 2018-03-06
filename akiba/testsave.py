b71bd375699a06dff5ee02eced8964f166303128
utylee <utylee@gmail.com>
2018-03-06 21:55:14 +0900

    akiba sh sql files added

---
+++ akiba/testsave.py
@@ -0,0 +1,6 @@
+#DIR = '/home/pi/media/3001/30-flask/python/selenium/akiba/static/torrents/watth.torrent.asdfateasdt.torrent'
+DIR = '/home/pi/media/3001/30-flask/python/selenium/akiba/static/torrents/mdyd-950-torrent.258668.torrent'
+with open(DIR, "wb") as w:
+    w.write('kkk'.encode())
+
+
