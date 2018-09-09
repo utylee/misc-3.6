19340dcc74350e73a9c29ceb581a277bbcad0efb
utylee <utylee@gmail.com>
2018-09-09 20:02:02 +0900

    sim.py added kor-eng server translation

--- sim.py
+++ sim.py
@@ -48,10 +48,20 @@ def sim_him(him):
         s = pyperclip.paste()
         f.write(s)
         '''
+
+    # ,(쉼표) 여부로 외부서버일 경우 분간하여 처리합니다
+
+    r = re.search('(.*)\,(.*)', him)
+
+    # 다른서버일 경우(쉼표있을 경우)
+    if (r):
+        eng = get_eng_name(r.group(1))
+        cmd = 'echo sksmsqnwk11 | sudo -S /home/utylee/temp/simc/engine/simc armory=kr,{},{}'.format(eng, r.group(2))
+    # 동일 아즈샤라 서버일 경우
+    else:
+        cmd = 'echo sksmsqnwk11 | sudo -S /home/utylee/temp/simc/engine/simc armory=kr,azshara,{}'.format(him)
     
     # simc를 돌립니다
-    cmd = 'echo sksmsqnwk11 | sudo -S /home/utylee/temp/simc/engine/simc armory=kr,azshara,{}'.format(him)
-    #print(cmd)
     result = subprocess.check_output(\
             cmd, shell=True)
     
@@ -78,16 +88,60 @@ def sim_him(him):
     print('\n{}\n{}'.format(him, lines[i+1]))
     
 
+def get_eng_name(r):
+    if (r == '데스윙'):
+        name = 'deathwing'
+    elif (r == '가로나'):
+        name = 'garona'
+    elif (r == '굴단'):
+        name = 'guldan'
+    elif (r == '노르간논'):
+        name = 'norgannon'
+    elif (r == '달라란'):
+        name = 'dalaran'
+    elif (r == '듀로탄'):
+        name = 'duratan'
+    elif (r == '렉사르'):
+        name = 'rexxar'
+    elif (r == '말퓨리온'):
+        name = 'malfurion'
+    elif (r == '불타는군단'):
+        name = 'burninglegion'
+    elif (r == '세나리우스'):
+        name = 'cenarius'
+    elif (r == '스톰레이지'):
+        name = 'stormrage'
+    elif (r == '아즈샤라'):
+        name = 'azshara'
+    elif (r == '알렉스트라자'):
+        name = 'alexstrasza'
+    elif (r == '와일드해머'):
+        name = 'wildhammer'
+    elif (r == '윈드러너'):
+        name = 'windrunner'
+    elif (r == '줄진'):
+        name = 'zuljin'
+    elif (r == '하이잘'):
+        name = 'hyjal'
+    elif (r == '헬스크림'):
+        name = 'hellscream'
+
+    #print(name)
+    return name
+
     
     
 if __name__ == "__main__":
 
     
     # 파라미터가 변수로 입력되면 그 변수를 넣어주고 그 사람의 전장정보실 정보를 이용해 심크를 돌립니다
-    if (len(sys.argv) > 1):
-        sim_him(sys.argv[1])
-    else:
-        sim_myself()
+    try:
+        if (len(sys.argv) > 1):
+            sim_him(sys.argv[1])
+        else:
+            sim_myself()
+    except:
+        pass
 
 '''
     try:
