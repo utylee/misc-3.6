a0ae5adeecbfb839487018645c69de7e363e3a63
utylee <utylee@gmail.com>
2018-09-25 16:03:56 +0900

    edted sim.py and freeze pip requirements.txt

--- sim.py
+++ sim.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 
-def sim_myself():
+def sim_myself(r):
     # 클립보드의 내용을 특정 파일에 기록한후
     
     with open("/home/utylee/temp/simc/engine/utylee.simc", "w") as f:
@@ -18,33 +18,39 @@ def sim_myself():
     
     # bytes 값을 스트링으로 변환합니다
     result = result.decode()
-    #print(result)
-    
-    # DPS에 해당하는 숫자열을 가져와서 표시합니다
-    lines = result.split('\n', 31)      # 굳이 전체를 다 파싱하진 않고 31열까지만 파싱합니다
-    
-    # 새 리스트를 선언합니다
-    new_lines = []
-    target_num = 0
-    
-    for i in range(0,30):
-        line = lines[i]
-        #m = re.search('DPS Ranking', line)
-        m = re.search('Player: ', line)
+
+    # 풀 출력
+    if(r == 1):
+        print('1')
+        print(result)
+   
+    else:
+        print('2')
+        # DPS에 해당하는 숫자열을 가져와서 표시합니다
+        lines = result.split('\n', 31)      # 굳이 전체를 다 파싱하진 않고 31열까지만 파싱합니다
         
-        if m:
-            #print('line [{}] : {}'.format(i, m.group()))
-            print('\n{}'.format(line))
-            target_num = i
-            break
-    
-    #찾은 다음열에 원하는 값이 들어있습니다.
-    #print('\n{}\n\n{}'.format(lines[i], lines[i+1]))
-    print('\n{}\n'.format(lines[i+1]))
-    
-    #print(new_lines)
-    
-    #print(' DPS : {} '.format(dps)) 
+        # 새 리스트를 선언합니다
+        new_lines = []
+        target_num = 0
+        
+        for i in range(0,30):
+            line = lines[i]
+            #m = re.search('DPS Ranking', line)
+            m = re.search('Player: ', line)
+            
+            if m:
+                #print('line [{}] : {}'.format(i, m.group()))
+                print('\n{}'.format(line))
+                target_num = i
+                break
+        
+        #찾은 다음열에 원하는 값이 들어있습니다.
+        #print('\n{}\n\n{}'.format(lines[i], lines[i+1]))
+        print('\n{}\n'.format(lines[i+1]))
+        
+        #print(new_lines)
+        
+        #print(' DPS : {} '.format(dps)) 
 
 def sim_him(him):
     '''
@@ -157,10 +163,13 @@ if __name__ == "__main__":
 async def main():
     # 파라미터가 변수로 입력되면 그 변수를 넣어주고 그 사람의 전장정보실 정보를 이용해 심크를 돌립니다
     try:
-        if (len(sys.argv) > 1):
-            sim_him(sys.argv[1])
+        if (len(sys.argv) == 2):
+            if (sys.argv[1] == 'f'):
+                sim_myself(1)
+            else:
+                sim_him(sys.argv[1])
         else:
-            sim_myself()
+            sim_myself(0)
     except:
         pass
 
--- test-server.py
+++ test-server.py
@@ -1,13 +1,13 @@
 import asyncio
 
 
-loop = asyncio.get_event_loop()
 
 
 @asyncio.coroutine
 def handle(reader, writer):
     data = yield from reader.read(100)
 
+loop = asyncio.get_event_loop()
 coro = asyncio.start_server(handle, '127.0.0.1', 8888, loop=loop) 
 server = loop.run_until_complete(coro)
 
