d211fd60d1d149e06611d4a6508fd993fe0358c5
utylee <utylee@gmail.com>
2018-09-26 20:09:05 +0900

    chg testsend to testclient.py

---
+++ test-client.py
@@ -0,0 +1,23 @@
+import asyncio
+import psutil
+
+
+
+
+async def proc():
+    fut = asyncio.open_connection('192.168.0.207', 1117, loop=loop)
+    try: 
+        reader, writer = await asyncio.wait_for(fut, timeout=3)
+    except:
+        print('exception maybe Timeouted')
+
+    #wbuf = 'what the fuck'
+    wbuf = psutil.cpu_percent()
+    print(wbuf)
+    writer.write(str(wbuf).encode())
+    await writer.drain()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(proc())
+
+
