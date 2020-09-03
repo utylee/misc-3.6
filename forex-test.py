f1e99aef464264fd50219c52eaf3dde03c81aa48
utylee <utylee@gmail.com>
2020-09-03 16:02:29 +0900

    fx.py change show default exchange rates

--- forex-test.py
+++ forex-test.py
@@ -6,22 +6,28 @@ from forex_python.converter import CurrencyRates
 
 
 if __name__ == "__main__":
-    try:
-        order = int(sys.argv[1])
-        price = int(sys.argv[2])
-        c = CurrencyRates()
-        #result = c.get_rate('USD', 'KRW')
-        # 달러를 원으로 변환합니다
-        if order == 0: 
-            result = c.convert('USD', 'KRW', int(price))
-            result = round(float(result), 2)
-            print(f'{result} 원')
-        else: 
-            result = c.convert('KRW', 'USD',int(price))
-            result = round(float(result), 2)
-            print(f'{result} dollars')
+    c = CurrencyRates()
 
-    except:
-        print(f'error occurred')
+    # 가격이 없을 경우 현재 달러/원 환율을 보여줍니다
+    if len(sys.argv) == 2: 
+        result = round(c.convert('USD', 'KRW', 1), 2)
+        print(f'{result} 원')
+    else:
+        try:
+            order = int(sys.argv[1])
+            price = int(sys.argv[2])
+            #result = c.get_rate('USD', 'KRW')
+            # 달러를 원으로 변환합니다
+            if order == 0: 
+                result = c.convert('USD', 'KRW', int(price))
+                result = round(float(result), 2)
+                print(f'{result} 원')
+            else: 
+                result = c.convert('KRW', 'USD',int(price))
+                result = round(float(result), 2)
+                print(f'{result} dollars')
+
+        except:
+            print(f'error occurred')
 
 #c.convert('
