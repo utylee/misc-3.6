d879a0bce486daeded044d63f5853bef0274b244
utylee <utylee@gmail.com>
2020-09-06 14:50:52 +0900

    fx fetch added function l to week data

--- forex-test.py
+++ forex-test.py
@@ -1,17 +1,35 @@
-
 import sys
+import datetime
+import time
 
 from forex_python.converter import CurrencyRates
-
 
 
 if __name__ == "__main__":
     c = CurrencyRates()
 
-    # 가격이 없을 경우 현재 달러/원 환율을 보여줍니다
+
+    # 아무 파라미터가 없을 경우 현재 달러/원 환율을 보여줍니다
     if len(sys.argv) == 2: 
         result = round(c.convert('USD', 'KRW', 1), 2)
         print(f'{result} 원')
+    # 가격 없이 order가 'l'이면 환율흐름을 보여줍니다
+    #elif len(sys.argv) == 3:  # 5가 아니어도 그냥 order값만 있는 것으로 판단하기로 합니다
+    elif len(sys.argv) == 3 and sys.argv[2] == 'l':  
+        today = datetime.date.today()
+        results = []
+        for i in range(0,7):
+            #l = dates.append(today - datetime.timedelta(days=i))
+            #print('\033[10C', end='')
+            print(f'\rfetching {i+1}...', end='')
+            time.sleep(0.01)
+            d = today - datetime.timedelta(days=i)
+            results.append(round(c.convert('USD', 'KRW', 1, d), 2))
+        print(f'\r Completed           ')
+        #print('')
+        for result in results:
+            print(f'{result} 원')
+
     else:
         try:
             order = int(sys.argv[1])
