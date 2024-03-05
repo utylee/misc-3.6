e9f0d8bb730a4a72fca9265e25a27ce487f9c2b9
utylee <utylee@gmail.com>
2024-03-06 03:39:59 +0900

    chg: requirements.txt update, youtube_uploading.py cleaning

--- youtube_uploading.py
+++ youtube_uploading.py
@@ -14,12 +14,12 @@ import re
 
 from ytstudio import Studio
 
-from apiclient.discovery import build
-from apiclient.errors import HttpError
-from apiclient.http import MediaFileUpload
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client.tools import argparser, run_flow
+# from apiclient.discovery import build
+# from apiclient.errors import HttpError
+# from apiclient.http import MediaFileUpload
+# from oauth2client.client import flow_from_clientsecrets
+# from oauth2client.file import Storage
+# from oauth2client.tools import argparser, run_flow
 
 import asyncio
 import aiohttp
@@ -796,123 +796,133 @@ https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
 
 
-def get_authenticated_service(args):
-    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-                                   scope=YOUTUBE_UPLOAD_SCOPE,
-                                   message=MISSING_CLIENT_SECRETS_MESSAGE)
+# def get_authenticated_service(args):
+#     flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
+#                                    scope=YOUTUBE_UPLOAD_SCOPE,
+#                                    message=MISSING_CLIENT_SECRETS_MESSAGE)
 
-    storage = Storage("%s-oauth2.json" % sys.argv[0])
-    credentials = storage.get()
+#     storage = Storage("%s-oauth2.json" % sys.argv[0])
+#     credentials = storage.get()
 
-    if credentials is None or credentials.invalid:
-        credentials = run_flow(flow, storage, args)
+#     if credentials is None or credentials.invalid:
+#         credentials = run_flow(flow, storage, args)
 
-    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-                 http=credentials.authorize(httplib2.Http()))
-
-
-def initialize_upload(youtube, options):
-    tags = None
-    if options.keywords:
-        tags = options.keywords.split(",")
-
-    body = dict(
-        snippet=dict(
-            title=options.title,
-            description=options.description,
-            tags=tags,
-            categoryId=options.category
-        ),
-        status=dict(
-            privacyStatus=options.privacyStatus
-        )
-    )
-
-    # Call the API's videos.insert method to create and upload the video.
-    insert_request = youtube.videos().insert(
-        part=",".join(list(body.keys())),
-        body=body,
-        # The chunksize parameter specifies the size of each chunk of data, in
-        # bytes, that will be uploaded at a time. Set a higher value for
-        # reliable connections as fewer chunks lead to faster uploads. Set a lower
-        # value for better recovery on less reliable connections.
-        #
-        # Setting "chunksize" equal to -1 in the code below means that the entire
-        # file will be uploaded in a single HTTP request. (If the upload fails,
-        # it will still be retried where it left off.) This is usually a best
-        # practice, but if you're using Python older than 2.6 or if you're
-        # running on App Engine, you should set the chunksize to something like
-        # 1024 * 1024 (1 megabyte).
-        media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True)
-    )
-
-    # resumable_upload(insert_request)
-    return resumable_upload(insert_request)
-
-# This method implements an exponential backoff strategy to resume a
-# failed upload.
+#     return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+#                  http=credentials.authorize(httplib2.Http()))
 
 
-def resumable_upload(insert_request):
-    # result라는 값을 리턴하도록 합니다. exception 이 발생하면 1을 반납합니다
-    result = 0
-    response = None
-    error = None
-    retry = 0
-    while response is None:
-        try:
-            print("Uploading file...")
-            log.info("Uploading file...")
-            status, response = insert_request.next_chunk()
-            if response is not None:
-                if 'id' in response:
-                    print(("Video id '%s' was successfully uploaded." %
-                          response['id']))
-                else:
-                    exit("The upload failed with an unexpected response: %s" % response)
-        except HttpError as e:
-            if e.resp.status in RETRIABLE_STATUS_CODES:
-                error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status,
-                                                                     e.content)
-            else:
-                raise
-        except RETRIABLE_EXCEPTIONS as e:
-            error = "A retriable error occurred: %s" % e
+#def initialize_upload(youtube, options):
+#    tags = None
+#    if options.keywords:
+#        tags = options.keywords.split(",")
 
-        if error is not None:
-            result = 1
-            print(error)
-            retry += 1
-            if retry > MAX_RETRIES:
-                exit("No longer attempting to retry.")
+#    body = dict(
+#        snippet=dict(
+#            title=options.title,
+#            description=options.description,
+#            tags=tags,
+#            categoryId=options.category
+#        ),
+#        status=dict(
+#            privacyStatus=options.privacyStatus
+#        )
+#    )
 
-            max_sleep = 2 ** retry
-            sleep_seconds = random.random() * max_sleep
-            print(("Sleeping %f seconds and then retrying..." % sleep_seconds))
-            time.sleep(sleep_seconds)
+#    # Call the API's videos.insert method to create and upload the video.
+#    insert_request = youtube.videos().insert(
+#        part=",".join(list(body.keys())),
+#        body=body,
+#        # The chunksize parameter specifies the size of each chunk of data, in
+#        # bytes, that will be uploaded at a time. Set a higher value for
+#        # reliable connections as fewer chunks lead to faster uploads. Set a lower
+#        # value for better recovery on less reliable connections.
+#        #
+#        # Setting "chunksize" equal to -1 in the code below means that the entire
+#        # file will be uploaded in a single HTTP request. (If the upload fails,
+#        # it will still be retried where it left off.) This is usually a best
+#        # practice, but if you're using Python older than 2.6 or if you're
+#        # running on App Engine, you should set the chunksize to something like
+#        # 1024 * 1024 (1 megabyte).
+#        media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True)
+#    )
 
-        return result
+#    # resumable_upload(insert_request)
+#    return resumable_upload(insert_request)
+
+## This method implements an exponential backoff strategy to resume a
+## failed upload.
+
+
+#def resumable_upload(insert_request):
+#    # result라는 값을 리턴하도록 합니다. exception 이 발생하면 1을 반납합니다
+#    result = 0
+#    response = None
+#    error = None
+#    retry = 0
+#    while response is None:
+#        try:
+#            print("Uploading file...")
+#            log.info("Uploading file...")
+#            status, response = insert_request.next_chunk()
+#            if response is not None:
+#                if 'id' in response:
+#                    print(("Video id '%s' was successfully uploaded." %
+#                          response['id']))
+#                else:
+#                    exit("The upload failed with an unexpected response: %s" % response)
+#        except HttpError as e:
+#            if e.resp.status in RETRIABLE_STATUS_CODES:
+#                error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status,
+#                                                                     e.content)
+#            else:
+#                raise
+#        except RETRIABLE_EXCEPTIONS as e:
+#            error = "A retriable error occurred: %s" % e
+
+#        if error is not None:
+#            result = 1
+#            print(error)
+#            retry += 1
+#            if retry > MAX_RETRIES:
+#                exit("No longer attempting to retry.")
+
+#            max_sleep = 2 ** retry
+#            sleep_seconds = random.random() * max_sleep
+#            print(("Sleeping %f seconds and then retrying..." % sleep_seconds))
+#            time.sleep(sleep_seconds)
+
+#        return result
 
 
 if __name__ == '__main__':
-    # argparser.add_argument("--file", required=True, help="Video file to upload")
-    argparser.add_argument("--file",  help="Video file to upload")
-    argparser.add_argument("--title", help="Video title", default="Test Title")
-    argparser.add_argument("--description", help="Video description",
-                           default="Test Description")
-    argparser.add_argument("--category", default="22",
-                           help="Numeric video category. " +
-                           "See https://developers.google.com/youtube/v3/docs/videoCategories/list")
-    argparser.add_argument("--keywords", help="Video keywords, comma separated",
-                           default="")
-    argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES,
-                           default=VALID_PRIVACY_STATUSES[0], help="Video privacy status.")
-    args = argparser.parse_args()
+    # # argparser.add_argument("--file", required=True, help="Video file to upload")
+    # argparser.add_argument("--file",  help="Video file to upload")
+    # argparser.add_argument("--title", help="Video title", default="Test Title")
+    # argparser.add_argument("--description", help="Video description",
+    #                        default="Test Description")
+    # argparser.add_argument("--category", default="22",
+    #                        help="Numeric video category. " +
+    #                        "See https://developers.google.com/youtube/v3/docs/videoCategories/list")
+    # argparser.add_argument("--keywords", help="Video keywords, comma separated",
+    #                        default="")
+    # argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES,
+    #                        default=VALID_PRIVACY_STATUSES[0], help="Video privacy status.")
+    # args = argparser.parse_args()
 
     # if not os.path.exists(args.file):
     #     exit("Please specify a valid file using the --file= parameter.")
     '''
     '''
+    # # youtube = get_authenticated_service(args)
+
+    # # 주기적으로 큐를 확인하면서 등록된 파일을 순차적으로 업로드 합니다
+    # try:
+    #     print(args.title)
+    #     log.info('-------------------------------------------')
+    #     log.info('started')
+    #     # initialize_upload(youtube, args)
+    # except HttpError as e:
+    #     print(("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)))
 
     # 로그설정입니다
     log = logging.getLogger('logger')
@@ -921,20 +931,10 @@ if __name__ == '__main__':
     log.addHandler(handler)
     log.setLevel(logging.DEBUG)
 
-    # youtube = get_authenticated_service(args)
-
-    # 주기적으로 큐를 확인하면서 등록된 파일을 순차적으로 업로드 합니다
-    try:
-        print(args.title)
-        log.info('-------------------------------------------')
-        log.info('started')
-        # initialize_upload(youtube, args)
-    except HttpError as e:
-        print(("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)))
 
     app = web.Application()
 
-    app['args'] = args
+    # app['args'] = args
     app['uploading'] = 0
     # app['youtube'] = youtube
     app['login_file'] = ''
