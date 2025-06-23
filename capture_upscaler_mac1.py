130979aaf4675dfa11e2a009e0454b161e6fa826
utylee <utylee@gmail.com>
2025-06-23 15:04:06 +0900

    chg: capture_upscaler_mac1.py developing

--- capture_upscaler_mac1.py
+++ capture_upscaler_mac1.py
@@ -57,7 +57,8 @@ VIDEO_EXT_LIST = ['mp4', 'mkv', 'webm', 'avi', 'mov', 'mpg', 'mpeg', 'wmv']
 # REMOTE_PATH = '/mnt/8001/97-Capture'
 # # REMOTE_PATH = '/mnt/clark/4002/00-MediaWorld-4002/97-Capture'
 
-JSON_PATH = '/Volumes/5003/login.json'
+# JSON_PATH = '/Volumes/5003/login.json'
+JSON_PATH = '/Users/utylee/Volumes/5003/login.json'
 
 # SUDO = 'sudo'
 # DAVINCI_PATH = '/mnt/c/Program Files/Blackmagic Design/DaVinci Resolve/Resolve.exe'
@@ -958,7 +959,7 @@ async def upscaling(app):
                 log.info(f'upscaling()::davinci_proc: {app["davinci_proc"]}')
                 log.info(f'upscaling()::wait for davinci resolve executing...')
                 # await asyncio.sleep(2)     # 실행시 10초정도는 기다려줘야하는 것 같습니다
-                await asyncio.sleep(10)     # 실행시 10초정도는 기다려줘야하는 것 같습니다
+                await asyncio.sleep(25)     # 실행시 10초정도는 기다려줘야하는 것 같습니다
                 # await app['davinci_proc'].wait()
 
                 # 업스케일을 실행합니다
